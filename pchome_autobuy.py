cb2f44c51df8fab0736dd47a9e0a15c5e6ca03fa
秉諺 吳 <brian.92308@gmail.com>
2021-11-07 13:24:21 +0800

    check

--- pchome_autobuy.py
+++ pchome_autobuy.py
@@ -7,10 +7,12 @@ import json
 import time
 import requests
 
-from selenium import webdriver
+from selenium import webdriver 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+
+
 
 """
 匯入欲搶購的連結、登入帳號、登入密碼及其他個資
@@ -20,30 +22,29 @@ from settings import (
     BuyerSSN, BirthYear, BirthMonth, BirthDay, multi_CVV2Num    
 )
 
+options = webdriver.ChromeOptions()  
+options.add_argument(CHROME_PATH)  
+
+driver = webdriver.Chrome(
+    executable_path="D:\Coding\Python\PChome-AutoBuy\chromedriver.exe", options=options)
+driver.set_page_load_timeout(120)
+
 def login():
     WebDriverWait(driver, 2).until(
-        expected_conditions.presence_of_element_located((By.ID, 'loginAcc'))
+        expected_conditions.presence_of_element_located((By.XPATH, '/html/body/div[6]/div[1]/div/div/div/div/div/div/div/div/div[2]/div/div/form/div[1]/div/div/input'))
     )
-    elem = driver.find_element_by_id('loginAcc')
+    elem = driver.find_element_by_xpath('/html/body/div[6]/div[1]/div/div/div/div/div/div/div/div/div[2]/div/div/form/div[1]/div/div/input')
     elem.clear()
     elem.send_keys(ACC)
-    elem = driver.find_element_by_id('loginPwd')
+    elem = driver.find_element_by_xpath('/html/body/div[6]/div[1]/div/div/div/div/div/div/div/div/div[2]/div/div/form/div[2]/div/div/input')
     elem.clear()
     elem.send_keys(PWD)
     WebDriverWait(driver, 20).until(
-        expected_conditions.element_to_be_clickable((By.ID, "btnLogin"))
+        expected_conditions.element_to_be_clickable((By.ID, "sign-in-btn"))
     )
-    driver.find_element_by_id('btnLogin').click()
+    driver.find_element_by_id('sign-in-btn').click()
     print('成功登入')
 
-def input_info(xpath, info):  # info = 個資
-    WebDriverWait(driver, 1).until(
-        expected_conditions.element_to_be_clickable(
-            (By.XPATH, xpath))
-    )
-    elem = driver.find_element_by_xpath(xpath)
-    elem.clear()
-    elem.send_keys(info)
 
 def click_button(xpath):
     WebDriverWait(driver, 20).until(
@@ -52,42 +53,23 @@ def click_button(xpath):
     )
     driver.find_element_by_xpath(xpath).click()
 
-def input_flow():
-    """
-    填入個資，若無法填入則直接填入信用卡背面安全碼 3 碼 (multi_CVV2Num)
-    """
-    try:
-        input_info(xpaths['BuyerSSN'], BuyerSSN)
-        input_info(xpaths['BirthYear'], BirthYear)
-        input_info(xpaths['BirthMonth'], BirthMonth)
-        input_info(xpaths['BirthDay'], BirthDay)
-    except:
-        print("Birth's info already filled in!")
-    finally:
-        input_info(xpaths['multi_CVV2Num'], multi_CVV2Num)
+def click_button_id(Btnid):
+    WebDriverWait(driver,20).until(
+        expected_conditions.element_to_be_clickable(
+            (By.ID,Btnid)
+        )
+    )
+    driver.find_element_by_id(Btnid).click()
 
-def get_product_id(url):
-    pattern = '(?<=prod/)(\w+-\w+)'
-    try:
-        product_id = re.findall(pattern, url)[0]
-        print(product_id)
-        return product_id
-    except Exception as e:
-        print(e.__class__.__name__, ': 取得商品 ID 錯誤！')
 
-def get_product_status(product_id):
-    api_url = f'https://ecapi.pchome.com.tw/ecshop/prodapi/v2/prod/button&id={product_id}'
-    resp = requests.get(api_url)
-    status = json.loads(resp.text)[0]['ButtonType']
-    return status
 
 """
 集中管理需要的 xpath
 """
 xpaths = {
-    'add_to_cart': r"//li[@id='ButtonContainer']/button",
-    'check_agree': r"//input[@name='chk_agree']",
-    'BuyerSSN': r"//input[@id='BuyerSSN']",
+    'add_to_cart': r"/html/body/div[6]/div[1]/div/div/div/div[2]/div[3]/div[2]/div[4]/div[7]/button[2]/div",
+    'check_agree': r"/html/body/div[6]/div/div/div/div/div[5]/div[1]/form/div/label/input",
+    'Pay_btn': r"/html/body/div[6]/div[1]/div/div/div/div[3]/div[5]/div[2]/section/div[2]/a",
     'BirthYear': r"//input[@name='BirthYear']",
     'BirthMonth': r"//input[@name='BirthMonth']",
     'BirthDay': r"//input[@name='BirthDay']",
@@ -98,100 +80,87 @@ xpaths = {
     # 'warning_msg': "//a[@id='warning-timelimit_btn_confirm']",  # 之後可能會有變動
 }
 
-def main():
-    driver.get(URL)
 
-    """
-    放入購物車
-    """
-    click_button(xpaths['add_to_cart'])
+driver.get(URL)
 
-    """
-    前往購物車
-    """
-    driver.get("https://ecssl.pchome.com.tw/sys/cflow/fsindex/BigCar/BIGCAR/ItemList")
+"""
+放入購物車
+"""
+click_button_id('#btn-variable-buy-now')
 
-    """
-    登入帳戶（若有使用 CHROME_PATH 記住登入資訊，第二次執行時可註解掉）
-    """
-    try:
-        login()
-    except:
-        print('Already Logged in!')
+"""
+前往購物車
+"""
+driver.get("https://www.huahuacomputer.com.tw/cart")
 
-    """
-    前往結帳 (一次付清) (要使用 JS 的方式 execute_script 點擊)
-    """
-    WebDriverWait(driver, 20).until(
-        expected_conditions.element_to_be_clickable(
-            (By.XPATH, "//li[@class='CC']/a[@class='ui-btn']"))
-    )
-    button = driver.find_element_by_xpath(
-        "//li[@class='CC']/a[@class='ui-btn']")
-    driver.execute_script("arguments[0].click();", button)
+"""
+登入帳戶（若有使用 CHROME_PATH 記住登入資訊，第二次執行時可註解掉）
+"""
+try:
+    login()
+except:
+    print('Already Logged in!')
 
-    """
-    LINE Pay 付款
-    """
-    # WebDriverWait(driver, 20).until(
-    #     expected_conditions.element_to_be_clickable(
-    #         (By.XPATH, "//li[@class='LIP']/a[@class='ui-btn line_pay']"))
-    # )
-    # button = driver.find_element_by_xpath(
-    #     "//li[@class='LIP']/a[@class='ui-btn line_pay']")
-    # driver.execute_script("arguments[0].click();", button)
+"""
+前往結帳 (一次付清) (要使用 JS 的方式 execute_script 點擊)
+"""
+WebDriverWait(driver, 20).until(
+    expected_conditions.element_to_be_clickable((By.XPATH, '/html/body/div[6]/div[1]/div/div/div/div[3]/div[5]/div[2]/section/div[2]/a'))
+)
+driver.find_element_by_xpath('/html/body/div[6]/div[1]/div/div/div/div[3]/div[5]/div[2]/section/div[2]/a').click()
 
-    """
-    點擊提示訊息確定 (有些商品可能不需要)
-    """
-    try:
-        WebDriverWait(driver, 1).until(
-            expected_conditions.element_to_be_clickable(
-                (By.XPATH, "//a[@id='warning-timelimit_btn_confirm']"))
-        )
-        button = driver.find_element_by_xpath("//a[@id='warning-timelimit_btn_confirm']")
-        driver.execute_script("arguments[0].click();", button)
-    except:
-        print('Warning message passed!')
+'''WebDriverWait(driver, 20).until(
+    expected_conditions.element_to_be_clickable(
+        (By.XPATH, "//li[@class='CC']/a[@class='ui-btn']"))
+)
+button = driver.find_element_by_xpath(
+    "//li[@class='CC']/a[@class='ui-btn']")
+driver.execute_script("arguments[0].click();", button)'''
 
-    """
-    填入個資
-    """
-    input_flow()
-
-    """
-    勾選同意（注意！若帳號有儲存付款資訊的話，不需要再次勾選，請註解掉！）
-    """
-    click_button(xpaths['check_agree'])
-
-    """
-    送出訂單 (要使用 JS 的方式 execute_script 點擊)
-    """
-    WebDriverWait(driver, 20).until(
-        expected_conditions.element_to_be_clickable(
-            (By.XPATH, "//a[@id='btnSubmit']"))
-    )
-    button = driver.find_element_by_xpath("//a[@id='btnSubmit']")
-    driver.execute_script("arguments[0].click();", button)
 
 
 """
-設定 option 可讓 chrome 記住已登入帳戶，成功後可以省去後續"登入帳戶"的程式碼
+點擊提示訊息確定 (有些商品可能不需要)
 """
-options = webdriver.ChromeOptions()  
-options.add_argument(CHROME_PATH)  
+'''try:
+    WebDriverWait(driver, 1).until(
+        expected_conditions.element_to_be_clickable(
+            (By.XPATH, "//a[@id='warning-timelimit_btn_confirm']"))
+    )
+    button = driver.find_element_by_xpath("//a[@id='warning-timelimit_btn_confirm']")
+    driver.execute_script("arguments[0].click();", button)
+except:
+    print('Warning message passed!')'''
 
-driver = webdriver.Chrome(
-    executable_path=DRIVER_PATH, chrome_options=options)
-driver.set_page_load_timeout(120)
+
+
+"""
+勾選同意（注意！若帳號有儲存付款資訊的話，不需要再次勾選，請註解掉！）
+"""
+click_button(xpaths['check_agree'])
+
+"""
+送出訂單 (要使用 JS 的方式 execute_script 點擊)
+"""
+
+WebDriverWait(driver, 20).until(
+    expected_conditions.element_to_be_clickable(
+        (By.ID, "place-order-btn"))
+)
+button = driver.find_element_by_id("place-order-btn")
+driver.execute_script("arguments[0].click();", button)
+
+
 
 """
 抓取商品開賣資訊，並嘗試搶購
 """
+'''
 curr_retry = 0
 max_retry = 5   # 重試達 5 次就結束程式，可自行調整
 wait_sec = 1    # 1 秒後重試，可自行調整秒數
-
+'''
+'''
 if __name__ == "__main__":
     product_id = get_product_id(URL)
     while curr_retry <= max_retry:  
@@ -204,3 +173,4 @@ if __name__ == "__main__":
             print('商品已開賣！')
             main()
             break
+'''
