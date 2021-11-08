b41819af7557bdf9a8fca4af6d7d486c2db874b4
秉諺 吳 <brian.92308@gmail.com>
2021-11-08 14:06:19 +0800

    check

--- pchome_autobuy.py
+++ pchome_autobuy.py
@@ -70,107 +70,81 @@ xpaths = {
     'add_to_cart': r"/html/body/div[6]/div[1]/div/div/div/div[2]/div[3]/div[2]/div[4]/div[7]/button[2]/div",
     'check_agree': r"/html/body/div[6]/div/div/div/div/div[5]/div[1]/form/div/label/input",
     'Pay_btn': r"/html/body/div[6]/div[1]/div/div/div/div[3]/div[5]/div[2]/section/div[2]/a",
-    'BirthYear': r"//input[@name='BirthYear']",
-    'BirthMonth': r"//input[@name='BirthMonth']",
-    'BirthDay': r"//input[@name='BirthDay']",
-    'multi_CVV2Num': r"//input[@name='multi_CVV2Num']"
     # 'pay_once': "//li[@class=CC]/a[@class='ui-btn']",
     # 'pay_line': "//li[@class=LIP]/a[@class='ui-btn line_pay']", 
     # 'submit': "//a[@id='btnSubmit']",
     # 'warning_msg': "//a[@id='warning-timelimit_btn_confirm']",  # 之後可能會有變動
 }
 
+def main():
+    driver.get(URL)
 
-driver.get(URL)
+    """
+    放入購物車
+    """
+    click_button_id('#btn-variable-buy-now')
 
-"""
-放入購物車
-"""
-click_button_id('#btn-variable-buy-now')
+    """
+    前往購物車
+    """
+    driver.get("https://www.huahuacomputer.com.tw/cart")
 
-"""
-前往購物車
-"""
-driver.get("https://www.huahuacomputer.com.tw/cart")
+    """
+    登入帳戶（若有使用 CHROME_PATH 記住登入資訊，第二次執行時可註解掉）
+    """
+    try:
+        login()
+    except:
+        print('Already Logged in!')
 
-"""
-登入帳戶（若有使用 CHROME_PATH 記住登入資訊，第二次執行時可註解掉）
-"""
-try:
-    login()
-except:
-    print('Already Logged in!')
-
-"""
-前往結帳 (一次付清) (要使用 JS 的方式 execute_script 點擊)
-"""
-WebDriverWait(driver, 20).until(
-    expected_conditions.element_to_be_clickable((By.XPATH, '/html/body/div[6]/div[1]/div/div/div/div[3]/div[5]/div[2]/section/div[2]/a'))
-)
-driver.find_element_by_xpath('/html/body/div[6]/div[1]/div/div/div/div[3]/div[5]/div[2]/section/div[2]/a').click()
-
-'''WebDriverWait(driver, 20).until(
-    expected_conditions.element_to_be_clickable(
-        (By.XPATH, "//li[@class='CC']/a[@class='ui-btn']"))
-)
-button = driver.find_element_by_xpath(
-    "//li[@class='CC']/a[@class='ui-btn']")
-driver.execute_script("arguments[0].click();", button)'''
-
-
-
-"""
-點擊提示訊息確定 (有些商品可能不需要)
-"""
-'''try:
-    WebDriverWait(driver, 1).until(
-        expected_conditions.element_to_be_clickable(
-            (By.XPATH, "//a[@id='warning-timelimit_btn_confirm']"))
+    """
+    前往結帳 (一次付清) (要使用 JS 的方式 execute_script 點擊)
+    """
+    WebDriverWait(driver, 20).until(
+        expected_conditions.element_to_be_clickable((By.XPATH, '/html/body/div[6]/div[1]/div/div/div/div[3]/div[5]/div[2]/section/div[2]/a'))
     )
-    button = driver.find_element_by_xpath("//a[@id='warning-timelimit_btn_confirm']")
+    driver.find_element_by_xpath('/html/body/div[6]/div[1]/div/div/div/div[3]/div[5]/div[2]/section/div[2]/a').click()
+
+
+    """
+    勾選同意（注意！若帳號有儲存付款資訊的話，不需要再次勾選，請註解掉！）
+    """
+    click_button(xpaths['check_agree'])
+
+    """
+    送出訂單 (要使用 JS 的方式 execute_script 點擊)
+    """
+    '''
+    WebDriverWait(driver, 20).until(
+        expected_conditions.element_to_be_clickable(
+            (By.ID, "place-order-btn"))
+    )
+    button = driver.find_element_by_id("place-order-btn")
     driver.execute_script("arguments[0].click();", button)
-except:
-    print('Warning message passed!')'''
-
-
-
-"""
-勾選同意（注意！若帳號有儲存付款資訊的話，不需要再次勾選，請註解掉！）
-"""
-click_button(xpaths['check_agree'])
-
-"""
-送出訂單 (要使用 JS 的方式 execute_script 點擊)
-"""
-
-WebDriverWait(driver, 20).until(
-    expected_conditions.element_to_be_clickable(
-        (By.ID, "place-order-btn"))
-)
-button = driver.find_element_by_id("place-order-btn")
-driver.execute_script("arguments[0].click();", button)
-
+    '''
 
 
 """
 抓取商品開賣資訊，並嘗試搶購
 """
-'''
+
 curr_retry = 0
 max_retry = 5   # 重試達 5 次就結束程式，可自行調整
 wait_sec = 1    # 1 秒後重試，可自行調整秒數
-'''
-'''
+
+
 if __name__ == "__main__":
-    product_id = get_product_id(URL)
-    while curr_retry <= max_retry:  
-        status = get_product_status(product_id)
-        if status != 'ForSale':
-            print('商品尚未開賣！')
-            curr_retry += 1
-            time.sleep(wait_sec)
-        else:
-            print('商品已開賣！')
+    while curr_retry<max_retry: 
+        driver.get(URL)
+        if(driver.find_element_by_id('#btn-variable-buy-now').is_enabled()):
+            print('商品開賣！')
             main()
             break
-'''
+        else:
+            print('商品尚未開賣！')
+            curr_retry+=1
+            time.sleep(wait_sec)
+            
+
+
+os.system("pause")
