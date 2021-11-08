1f9df2bfdea761b701497a45a3df0f8d227592ed
秉諺 吳 <brian.92308@gmail.com>
2021-11-08 14:21:43 +0800

    adding loop and change URL

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-URL = "https://www.huahuacomputer.com.tw/products/msi-%E5%BE%AE%E6%98%9F-gf63-10scsr-1644tw-%E9%BB%91-%E9%9B%BB%E7%AB%B6%E7%AD%86%E9%9B%BB"
+URL = "https://www.huahuacomputer.com.tw/products/gigabyte-%E6%8A%80%E5%98%89-aorus-15g-yb-%E6%A9%9F%E6%A2%B0%E8%BB%B8%E9%9B%BB%E7%AB%B6%E7%AD%86%E9%9B%BB-1"
 DRIVER_PATH = "D:\Coding\Python\PChome-AutoBuy\chromedriver.exe"
 CHROME_PATH = r"--user-data-dir=C:\\Users\brian\AppData\\Local\\Google\\Chrome\\User Data\Default"  # 可透過網址列輸入 chrome://version/ 找到
 
@@ -9,6 +9,3 @@ CHROME_PATH = r"--user-data-dir=C:\\Users\brian\AppData\\Local\\Google\\Chrome\\
 # 請注意！以下皆為機密個資，請小心謹慎，勿上傳至公開平台
 ACC = "brian.92308@gmail.com"
 PWD = "wu920308"
-BuyerSSN = "身分證字號"
-BirthYear, BirthMonth, BirthDay = "民國出生年", "月", "日"
-multi_CVV2Num = "信用卡檢查碼（背面三碼）"
